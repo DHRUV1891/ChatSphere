063f9f07b0f967fad79f6bd90bc4c7a4f36cc0dc
DHRUV1891 <96827779+DHRUV1891@users.noreply.github.com>
2024-05-31 12:29:51 +0530

    MONGODB setup created and updated

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
+import { login , logout , signup } from  './auth.controller.js'
 
 const router = express.Router();
 
-router.get('/login', (req, res) => {
-    res.send("Login Route");
-});
+router.post('/login', login);
+router.post('/signup', signup);
+router.post('/logout', logout);
 
 export default router ;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from "dotenv";
 import authRoutes from './routes/auth.routes.js'
+import connectToMongoDB from './db/connectToMongoDB.js';
 
 
 const app = express();
@@ -13,5 +14,7 @@ app.get('/', (req, res) => { res.send("Welcome to ChatSphere!");
 app.use('/api/auth',authRoutes)
 
 
- app.listen(PORT,()=> console.log(`Server running on port ${PORT}`)
+ app.listen(PORT,()=>{ 
+    connectToMongoDB();
+    console.log(`Server running on port ${PORT}`)}
 );
